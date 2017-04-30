d86d457e24876ae7add7ba63cfcfee8c26b619cd
hayashiki <peperoncino.pop@gmail.com>
2017-04-30 11:32:38 +0700

    mod-email-func

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,9 @@ var functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
 
 
 const nodemailer = require('nodemailer');
@@ -16,6 +16,8 @@ const gmailEmail = encodeURIComponent(functions.config().gmail.email);
 const gmailPassword = encodeURIComponent(functions.config().gmail.password);
 const mailTransport = nodemailer.createTransport(
     `smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+const mailto = "h.masako123@gmail.com"
+const mailfrom = '"英会話教室P.E.C 松江" <noreply@firebase.com>'
 
 // Sends an email confirmation when a user changes his mailing list subscription.
 exports.sendEmailConfirmation = functions.database.ref('/contact/{uid}').onWrite(event => {
@@ -27,15 +29,14 @@ exports.sendEmailConfirmation = functions.database.ref('/contact/{uid}').onWrite
   }
 
   const mailOptions = {
-    from: '"英会話教室P.E.C 松江" <noreply@firebase.com>',
-    // to: "peperoncino.pop@gmail.com"
-    to: val.email
+    from: mailfrom,
+    to: mailto
   };
 
   // The user just subscribed to our newsletter.
   if (val.subscribedToMailingList) {
     mailOptions.subject = '英会話教室P.E.C 松江への問い合わせ';
-    mailOptions.text = val.text1;
+    mailOptions.text = `英会話教室P.E.C 松江へ問い合わせがありました。\n問い合わせ日時: ${request_time}\nお名前: ${val.username}\nemail: ${val.email}\n電話番号： ${val.tel}\n問い合わせ内容： ${val.content}`;
     return mailTransport.sendMail(mailOptions).then(() => {
       // console.log('New subscription confirmation email sent to:', val.email);
     });
